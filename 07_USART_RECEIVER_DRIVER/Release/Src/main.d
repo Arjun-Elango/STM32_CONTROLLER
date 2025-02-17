dc3cf8d72d72c608e6220767ec035d991bac17d8
Arjun-Elango <136604404+Arjun-Elango@users.noreply.github.com>
2025-02-17 22:31:24 +0530

    Add files via upload

---
+++ 07_USART_RECEIVER_DRIVER/Release/Src/main.d
@@ -0,0 +1,17 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/PY-DOM/STM32CubeIDE/workspace_1.17.0/F4_Header/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Users/PY-DOM/STM32CubeIDE/workspace_1.17.0/F4_Header/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h \
+ C:/Users/PY-DOM/STM32CubeIDE/workspace_1.17.0/F4_Header/CMSIS/Include/core_cm4.h \
+ C:/Users/PY-DOM/STM32CubeIDE/workspace_1.17.0/F4_Header/CMSIS/Include/cmsis_version.h \
+ C:/Users/PY-DOM/STM32CubeIDE/workspace_1.17.0/F4_Header/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/PY-DOM/STM32CubeIDE/workspace_1.17.0/F4_Header/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/PY-DOM/STM32CubeIDE/workspace_1.17.0/F4_Header/CMSIS/Include/mpu_armv7.h \
+ C:/Users/PY-DOM/STM32CubeIDE/workspace_1.17.0/F4_Header/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+C:/Users/PY-DOM/STM32CubeIDE/workspace_1.17.0/F4_Header/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+C:/Users/PY-DOM/STM32CubeIDE/workspace_1.17.0/F4_Header/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h:
+C:/Users/PY-DOM/STM32CubeIDE/workspace_1.17.0/F4_Header/CMSIS/Include/core_cm4.h:
+C:/Users/PY-DOM/STM32CubeIDE/workspace_1.17.0/F4_Header/CMSIS/Include/cmsis_version.h:
+C:/Users/PY-DOM/STM32CubeIDE/workspace_1.17.0/F4_Header/CMSIS/Include/cmsis_compiler.h:
+C:/Users/PY-DOM/STM32CubeIDE/workspace_1.17.0/F4_Header/CMSIS/Include/cmsis_gcc.h:
+C:/Users/PY-DOM/STM32CubeIDE/workspace_1.17.0/F4_Header/CMSIS/Include/mpu_armv7.h:
+C:/Users/PY-DOM/STM32CubeIDE/workspace_1.17.0/F4_Header/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
---
+++ 07_USART_RECEIVER_DRIVER/Src/main.c
@@ -0,0 +1,23 @@
+/*USART Transmit a numeric value Using Modular code and __io_putchar function
+ * the actual function written in "USART.C"
+ * Created a header for calling the function
+ * by using this type of method we can reduce the repeated code
+ * for the same function for different operation
+ *also for simple and structured Main.c file
+ */
+#include "stm32f4xx.h"
+#include "usart.h"
+#include <stdio.h>
+
+int main(void){
+	usart_init();
+	int ch; // the receiving value
+	while(1)
+	{
+		ch = usart_read(); //reading the value
+		usart_write(ch); // write the same value
+        for(int i=0;i<100000;i++);
+
+    }
+
+}
