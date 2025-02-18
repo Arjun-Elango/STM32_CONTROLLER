21c9b86ff66be587a92149fd2e488c84d705954c
Arjun-Elango <136604404+Arjun-Elango@users.noreply.github.com>
2025-02-18 07:16:36 +0530

    Create usart.h

---
+++ USART/TRANSMITTER/String/inc/usart.h
@@ -0,0 +1,14 @@
+/*
+ * usart.h
+ *
+ *  Created on: Feb 16, 2025
+ *      Author: PY-DOM
+ */
+
+#ifndef USART_H_
+#define USART_H_
+
+void usart_init(void);
+void usart_write(int ch);
+
+#endif /* USART_H_ */
