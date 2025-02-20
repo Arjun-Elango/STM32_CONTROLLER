2678ae18afcc71719d1f9d4548cca04a67b014cb
Arjun-Elango <136604404+Arjun-Elango@users.noreply.github.com>
2025-02-20 15:10:24 +0530

    Create systick.h

---
+++ systick_interrupt_driver/inc/systick.h
@@ -0,0 +1,14 @@
+/*
+ * systick.h
+ *
+ *  Created on: Feb 19, 2025
+ *      Author: acer
+ */
+
+#ifndef SYSTICK_H_
+#define SYSTICK_H_
+
+void systickMs(int delay);
+void systick_interrupt_init(void);
+
+#endif /* SYSTICK_H_ */
