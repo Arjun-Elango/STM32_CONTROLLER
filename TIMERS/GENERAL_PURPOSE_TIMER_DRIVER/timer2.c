2bc40716dbd32e2958b34bbc572ab4d3bfceea27
Arjun-Elango <136604404+Arjun-Elango@users.noreply.github.com>
2025-02-24 13:42:27 +0530

    Create timer2.c

---
+++ TIMERS/GENERAL_PURPOSE_TIMER_DRIVER/timer2.c
@@ -0,0 +1,27 @@
+/*process for 1hz init function
+ * Enable clock to timer2 (APB1ENR)
+ * set Prescalar value (PSC)
+ * set automatic reload register(ARR)
+ * clear counter register(CNT)
+ * Enable timer(CR1)*/
+
+#include "stm32f4xx.h"
+#define TIMER2EN (1U<<0)
+#define PSC_VALUE_10KHZ  (1600-1)
+#define ARR_VALUE_1HZ (10000-1)
+#define CEN  (1U<<0)
+
+void timer2_1hz_init(void){
+	// enable clock
+	RCC->APB1ENR |= TIMER2EN ;
+	//set prescalar register for 10khz counter
+	TIM2->PSC = PSC_VALUE_10KHZ;
+
+	TIM2->ARR = ARR_VALUE_1HZ;
+
+	/*clear counter Register*/
+	TIM2->CNT =0;
+
+	/*enable timer CONTROL REGISTER 1 TO enable bit0 should 1*/
+	TIM2->CR1 |=CEN;
+}
