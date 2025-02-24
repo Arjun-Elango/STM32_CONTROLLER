463f324c0ad69f394062e4d26bc2ddb0fa286cdd
Arjun-Elango <136604404+Arjun-Elango@users.noreply.github.com>
2025-02-24 13:51:59 +0530

    Create timer2.h

---
+++ TIMERS/GENERAL_PURPOSE_INTERRUPT_DRIVER/inc/timer2.h
@@ -0,0 +1,15 @@
+/*
+ * Timer.h
+ *
+ *  Created on: Feb 22, 2025
+ *      Author: PY-DOM
+ */
+
+#ifndef TIMER_H_
+#define TIMER_H_
+
+void timer2_1hz_init(void);
+void timer2_1hz_interrupt_init(void);
+
+#endif /* TIMER_H_ */
+
