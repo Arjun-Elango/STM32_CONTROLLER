b3b2b7cb5b3da5737001a226027440c0fade607a
Arjun-Elango <136604404+Arjun-Elango@users.noreply.github.com>
2025-03-01 09:18:17 +0530

    Create timer.h

---
+++ TIMERS/input_compare_mode/inc/timer.h
@@ -0,0 +1,16 @@
+/*
+ * Timer.h
+ *
+ *  Created on: Feb 22, 2025
+ *      Author: PY-DOM
+ */
+
+#ifndef TIMER_H_
+#define TIMER_H_
+
+void timer2_1hz_init(void);
+void timer2_1hz_interrupt_init(void);
+void timer2_output_compare_init(void);
+void timer3_input_compare_init(void);
+
+#endif /* TIMER_H_ */
