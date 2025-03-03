2855a39242c44ad3e2c9c8ea86f36219d9b1c15c
Arjun-Elango <136604404+Arjun-Elango@users.noreply.github.com>
2025-03-03 05:42:25 +0530

    Create main.c

---
+++ SPI/src/main.c
@@ -0,0 +1,32 @@
+#include "stm32f4xx.h"
+#include "usart.h"
+#include "spi.h"
+#include "adxl.h"
+#include <stdio.h>
+#include <stdint.h>
+
+#define DATA_START_ADDR (0x32)
+
+int16_t x,y,z;
+
+uint8_t data_rec[6];
+
+int main (void){
+	/*USART init*/
+	usart_init();
+
+	/*initialize adxl*/
+	adxl_init();
+	while(1)
+	{
+		adxl_read(DATA_START_ADDR, data_rec);
+		x = ((data_rec[1]<<8) | (data_rec[0]));
+		y = ((data_rec[3]<<8) | (data_rec[2]));
+		z = ((data_rec[5]<<8) | (data_rec[4]));
+
+		printf("X = %d\r\n",(int)x);
+		printf("Y = %d\r\n",(int)y);
+		printf("Z = %d\r\n",(int)z);
+		for(int i=0; i<10000000;i++);
+	}
+}
